6d0613410e0dd959f10104d43dc237eef8628483
Istvan Feher <62614194+128478203-myseneca@users.noreply.github.com>
2023-06-07 10:25:17 -0400

    Created function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+
+# A function that will write a text file to your PC with your name
+def write_text_file_with_name():
+    name = input("Enter your name: ")
+    filename = "output.txt"
+    
+    with open(filename, "w") as file:
+        file.write(name)
+    
+    print("File was created with the following name", filename)
+
+write_text_file_with_name()
